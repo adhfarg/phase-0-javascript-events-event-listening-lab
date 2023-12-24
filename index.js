21ad4d3a2b5b0bcfe131b1632ad0d43d08adbf9e
adhfarg <153331802+adhfarg@users.noreply.github.com>
2023-12-24 16:55:55 -0500

    ”completed”

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button');
+  
+    function handleClick() {
+      console.log('Button clicked!');
+    }
+  
+    button.addEventListener('click', handleClick);
+  }
